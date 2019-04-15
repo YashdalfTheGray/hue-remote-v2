4adb0c5417409d8b0c993949a204cb7e7d534747
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2019-04-15 09:48:41 -0700

    refactored responses to models

--- handlerfuncs/status.go
+++ handlerfuncs/status.go
@@ -4,7 +4,7 @@ import (
 	"encoding/json"
 	"net/http"
 
-	"github.com/yashdalfthegray/hue-remote-v2/responses"
+	"github.com/yashdalfthegray/hue-remote-v2/models"
 )
 
 // Status sends out the status of the server as a health check
@@ -12,7 +12,7 @@ func Status(w http.ResponseWriter, req *http.Request) {
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 	w.WriteHeader(http.StatusOK)
 
-	if err := json.NewEncoder(w).Encode(responses.Status{
+	if err := json.NewEncoder(w).Encode(models.Status{
 		Status: "ok",
 	}); err != nil {
 		panic(err)
