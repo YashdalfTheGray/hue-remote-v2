62b736e39e5619b432a07b30af3fd2faded33e73
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2019-04-25 09:06:59 -0700

    added a constructor function for status

--- models/status.go
+++ models/status.go
@@ -1,6 +1,13 @@
 package models
 
-// Status is the server status response payload, gets sent back on GET /
-type Status struct {
+// StatusResponse is the server status response payload,
+// gets sent back on GET /
+type StatusResponse struct {
 	Status string `json:"status"`
+}
+
+// NewStatusResponse creates a new response to send out with the
+// given server ID
+func NewStatusResponse() StatusResponse {
+	return StatusResponse{Status: "ok"}
 }
