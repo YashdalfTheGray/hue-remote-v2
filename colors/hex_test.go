4f5c169f05dc522a6a46ddda6220b49e1cae1fb5
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2019-04-29 09:30:14 -0700

    added tests for hex color strings, need to make RGB conversion stricter

---
+++ colors/hex_test.go
@@ -0,0 +1,55 @@
+package colors_test
+
+import (
+	"testing"
+
+	"github.com/yashdalfthegray/hue-remote-v2/colors"
+)
+
+func TestHexToRGB(t *testing.T) {
+	testCases := []struct {
+		desc string
+		in   colors.HexCode
+		out  colors.RGB
+		err  bool
+	}{
+		{
+			desc: "converts a hex string to RGB",
+			in:   colors.NewHexCode("#deadaf"),
+			out:  colors.NewRGB(222, 173, 175),
+			err:  false,
+		},
+		{
+			desc: "errors on string without the # sign",
+			in:   colors.NewHexCode("deadaf"),
+			out:  colors.NewRGB(0, 0, 0),
+			err:  true,
+		},
+		{
+			desc: "errors on malformed color string",
+			in:   colors.NewHexCode("#234092834"),
+			out:  colors.NewRGB(0, 0, 0),
+			err:  true,
+		},
+		{
+			desc: "errors on invalid characters",
+			in:   colors.NewHexCode("#jklmno"),
+			out:  colors.NewRGB(0, 0, 0),
+			err:  true,
+		},
+	}
+	for _, tC := range testCases {
+		t.Run(tC.desc, func(t *testing.T) {
+			result, err := tC.in.ToRGB()
+			if !tC.err && err != nil {
+				t.Errorf(err.Error())
+			}
+			if tC.err && err == nil {
+				t.Errorf("expecting error to not be nil")
+			}
+			if result.String() != tC.out.String() {
+				t.Errorf("expected %s but got %s", tC.out, result.String())
+			}
+		})
+	}
+}
