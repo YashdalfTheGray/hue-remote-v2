090ae6846eec7be1ee5b80f020d7fb0d293b160a
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2019-05-08 08:18:12 -0700

    started adding tests for rgb color conversion

--- colors/rgb_test.go
+++ colors/rgb_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/yashdalfthegray/hue-remote-v2/colors"
+	"github.com/yashdalfthegray/hue-remote-v2/utils"
 )
 
 func TestNewRGB(t *testing.T) {
@@ -31,6 +32,27 @@ func TestNewRGB(t *testing.T) {
 	}
 }
 
+func TestRGBString(t *testing.T) {
+	testCases := []struct {
+		desc string
+		in   colors.RGB
+		out  string
+	}{
+		{
+			desc: "builds a string out of an RGB color",
+			in:   colors.NewRGB(200, 90, 50),
+			out:  "rgb(200, 90, 50)",
+		},
+	}
+	for _, tC := range testCases {
+		t.Run(tC.desc, func(t *testing.T) {
+			if tC.in.String() != tC.out {
+				t.Errorf("Expected %s but got %s", tC.out, tC.in.String())
+			}
+		})
+	}
+}
+
 func TestRGBToHexCode(t *testing.T) {
 	testCases := []struct {
 		desc string
@@ -47,6 +69,33 @@ func TestRGBToHexCode(t *testing.T) {
 		t.Run(tC.desc, func(t *testing.T) {
 			if tC.in.ToHexCode() != tC.out {
 				t.Errorf("Expected %s but got %s", tC.in.ToHexCode(), tC.out)
+			}
+		})
+	}
+}
+
+func TestRGBToHSL(t *testing.T) {
+	testCases := []struct {
+		desc string
+		in   colors.RGB
+		out  colors.HSL
+	}{
+		{
+			desc: "converts a non-gray RGB color to HSL",
+			in:   colors.NewRGB(13, 166, 242),
+			out:  utils.Must(colors.NewHSL(200, 90, 50)).(colors.HSL),
+		},
+		{
+			desc: "converts a gray RGB color to HSL",
+			in:   colors.NewRGB(128, 128, 128),
+			out:  utils.Must(colors.NewHSL(0, 0, 50)).(colors.HSL),
+		},
+	}
+	for _, tC := range testCases {
+		t.Run(tC.desc, func(t *testing.T) {
+			result := tC.in.ToHSL()
+			if result.String() != tC.out.String() {
+				t.Errorf("Expected %s but got %s", tC.out.String(), result.String())
 			}
 		})
 	}
