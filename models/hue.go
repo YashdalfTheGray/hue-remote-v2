1e5ff6e511738abf477bac95deaee7827f9f984b
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2019-05-31 02:27:36 -0700

    added the basic hue types for actions and states

--- models/hue.go
+++ models/hue.go
@@ -3,14 +3,28 @@ package models
 // HueAction is a struct that represents the request going to
 // the Hue bridge from the user
 type HueAction struct {
-	Effect    string `json:"effect"`
-	Colormode string `json:"colormode"`
+	Effect    string    `json:"effect"`
+	Colormode string    `json:"colormode"`
+	Alert     string    `json:"alert"`
+	On        bool      `json:"on"`
+	Hue       uint16    `json:"hue"`
+	Sat       uint8     `json:"sat"`
+	Bri       uint8     `json:"bri"`
+	XY        []float64 `json:"xy"`
+	CT        uint16    `json:"ct"`
 }
 
 // HueState is a struct that represents the response coming
 // back from the Hue bridge
 type HueState struct {
-	Effect    string `json:"effect"`
-	Colormode string `json:"colormode"`
-	Reachable string `json:"reachable"`
+	Effect    string    `json:"effect"`
+	Colormode string    `json:"colormode"`
+	Reachable string    `json:"reachable"`
+	Alert     string    `json:"alert"`
+	On        bool      `json:"on"`
+	Hue       uint16    `json:"hue"`
+	Sat       uint8     `json:"sat"`
+	Bri       uint8     `json:"bri"`
+	XY        []float64 `json:"xy"`
+	CT        uint16    `json:"ct"`
 }
