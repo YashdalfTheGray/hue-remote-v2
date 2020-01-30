20ff209689b0f98e1f668f12dbef7f6ff97750cc
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2020-01-30 08:16:06 -0800

    added a redis server found property to the status struct

--- models/status.go
+++ models/status.go
@@ -3,7 +3,8 @@ package models
 // StatusResponse is the server status response payload,
 // gets sent back on GET /
 type StatusResponse struct {
-	Status string `json:"status"`
+	Status           string `json:"status"`
+	RedisServerFound bool   `json:"redisServerFound"`
 }
 
 // NewStatusResponse creates a new response to send out with the
