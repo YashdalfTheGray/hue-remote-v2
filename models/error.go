065606b1e35601fb28f58fc8e56a55d5f14f7f09
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2020-02-03 08:20:16 -0800

    added an argument error to standardize

--- models/error.go
+++ models/error.go
@@ -1,7 +1,9 @@
 package models
 
+import "errors"
+
 // ErrorResponse is the server status response payload,
-// gets sent back on GET /
+// gets sent back on any request
 type ErrorResponse struct {
 	Status  string `json:"status"`
 	Message string `json:"message"`
@@ -11,4 +13,15 @@ type ErrorResponse struct {
 // given server ID
 func NewErrorResponse(err string) ErrorResponse {
 	return ErrorResponse{Status: "error", Message: err}
+}
+
+// NewArgumentError created a new error instance with the right
+// message and returns it, optionally accounting for if an
+// argument was passed in.
+func NewArgumentError(arg string) error {
+	if arg == "" {
+		return errors.New("The arguments given are invalid")
+	}
+
+	return errors.New("Argument " + arg + " is invalid")
 }
