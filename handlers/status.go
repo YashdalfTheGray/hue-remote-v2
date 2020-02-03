65b67cbd699c4d13db9fc7dececd854c74185d23
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2020-02-03 07:37:06 -0800

    extracted the redis server availability check out to a function

--- handlers/status.go
+++ handlers/status.go
@@ -15,19 +15,8 @@ import (
 // Status sends out the status of the server as a health check
 func Status(w http.ResponseWriter, req *http.Request) {
 	currentStatus := models.NewStatusResponse()
-	redisURL, _ := os.LookupEnv("REDIS_URL")
-	urlPieces := strings.Split(redisURL, "://")
-	client, poolErr := radix.NewPool("tcp", urlPieces[1], 10)
-	if poolErr != nil {
-		currentStatus.RedisServerFound = false
-	} else {
-		response, respErr := utils.RedisPing(client)
-		if respErr != nil {
-			currentStatus.RedisServerFound = false
-		} else {
-			currentStatus.RedisServerFound = (response == "PONG")
-		}
-	}
+
+	currentStatus.RedisServerFound = isRedisServerAvailable()
 
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 	w.WriteHeader(http.StatusOK)
@@ -35,4 +24,20 @@ func Status(w http.ResponseWriter, req *http.Request) {
 	if err := json.NewEncoder(w).Encode(currentStatus); err != nil {
 		panic(err)
 	}
+}
+
+func isRedisServerAvailable() bool {
+	redisURL, _ := os.LookupEnv(utils.EnvRedisURL)
+	urlPieces := strings.Split(redisURL, "://")
+	client, poolErr := radix.NewPool("tcp", urlPieces[1], 10)
+	if poolErr != nil {
+		return false
+	}
+
+	response, respErr := utils.RedisPing(client)
+	if respErr != nil {
+		return false
+	}
+
+	return (response == "PONG")
 }
